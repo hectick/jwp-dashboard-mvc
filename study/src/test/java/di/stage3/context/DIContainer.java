25ae2a344d0dbdb7b94824d663155516a3817144
hectick <ch.yeondev@gmail.com>
2023-09-26 18:12:56 +0900

    feat: test3 돌아가게 구현

--- study/src/test/java/di/stage3/context/DIContainer.java
+++ study/src/test/java/di/stage3/context/DIContainer.java
@@ -1,5 +1,8 @@
 package di.stage3.context;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -10,11 +13,52 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = createBeans(classes);
+        this.beans.forEach(this::setFields);
+    }
+
+    // 기본 생성자로 빈을 생성한다.
+    private Set<Object> createBeans(final Set<Class<?>> classes) {
+        Set<Object> o = new HashSet<>();
+        try{
+            for (Class<?> aClass : classes) {
+                final Constructor<?> constructor = aClass.getDeclaredConstructor();
+                constructor.setAccessible(true);
+                final Object instance = constructor.newInstance();
+                o.add(instance);
+            }
+        }catch(Exception e) {
+            e.printStackTrace();
+            throw new IllegalStateException("객체 생성 실패");
+        }
+        return o;
+    }
+
+    private void setFields(final Object bean) {
+        try{
+            final Field[] fields = bean.getClass().getDeclaredFields();
+            for (Field field : fields) {
+                final Object fieldInstance = beans.stream()
+                        .filter(each -> field.getType().isInstance(each))
+                        .findFirst()
+                        .orElse(null);
+                if(fieldInstance != null) {
+                    field.setAccessible(true);
+                    field.set(bean, fieldInstance);
+                }
+            }
+        }catch(Exception e) {
+            e.printStackTrace();
+            throw new IllegalStateException("필드 대입 실패");
+        }
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return beans.stream()
+                .filter(aClass::isInstance)
+                .map(bean -> (T) bean)
+                .findFirst()
+                .orElse(null);
     }
 }
